9fda03a4e5fb1524792c70e61cdb18c675dfb65b
brstrutt <ben.strutt@hotmail.co.uk>
2025-12-10 11:44:00 +0000

    add buttons that show/hide the bellsprounts and spoinks

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 
 document.addEventListener("DOMContentLoaded", async function () {
     document.getElementById("toggle_pixel_grid")?.addEventListener('click', togglePixelGridVisible);
+    document.getElementById("toggle_bellsprout")?.addEventListener('click', toggleBellsproutVisible);
+    document.getElementById("toggle_spoink")?.addEventListener('click', toggleSpoinkVisible);
 });
 
 function togglePixelGridVisible() {
-    console.log("CLICKYCLICKY");
     const grid_elements = document.getElementsByClassName("pixel_grid");
     if(grid_elements) {
         for(const grid of grid_elements) {
             grid.hidden = !grid.hidden;
+        }
+    }
+}
+
+function toggleBellsproutVisible() {
+    const bellsprouts = document.getElementsByClassName("bellsprout");
+    if(bellsprouts) {
+        for(const sprout of bellsprouts) {
+            sprout.hidden = !sprout.hidden;
+        }
+    }
+}
+
+function toggleSpoinkVisible() {
+    const spoinks = document.getElementsByClassName("spoink");
+    if(spoinks) {
+        for(const spoink of spoinks) {
+            spoink.hidden = !spoink.hidden;
         }
     }
 }
