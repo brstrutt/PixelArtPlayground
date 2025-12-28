740f1382acb4fef0919793772258f9d9bcfdf5e1
brstrutt <ben.strutt@hotmail.co.uk>
2025-12-28 17:02:48 +0000

    Change pixel grid toggle button to use CSS instead of Javascript

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 
 document.addEventListener("DOMContentLoaded", async function () {
-    document.getElementById("toggle_pixel_grid")?.addEventListener('click', togglePixelGridVisible);
     document.getElementById("toggle_bellsprout")?.addEventListener('click', toggleBellsproutVisible);
     document.getElementById("toggle_spoink")?.addEventListener('click', toggleSpoinkVisible);
 });
-
-function togglePixelGridVisible() {
-    const grid_elements = document.getElementsByClassName("pixel_grid");
-    if(grid_elements) {
-        for(const grid of grid_elements) {
-            grid.hidden = !grid.hidden;
-        }
-    }
-}
 
 function toggleBellsproutVisible() {
     const bellsprouts = document.querySelectorAll("main .bellsprout");
